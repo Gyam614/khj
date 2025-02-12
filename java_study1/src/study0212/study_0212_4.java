4a35b2bc2ffaaa3ba3be16b4feb1daa51a825963
khj <hato614@gmail.com>
2025-02-12 12:56:27 +0900

    study_0212_2차배열

---
+++ java_study1/src/study0212/study_0212_4.java
@@ -0,0 +1,15 @@
+package study0212;
+
+import java.util.Scanner;
+
+public class study_0212_4 {
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[][][] arr = new int[3][2][2];
+	}
+
+}
